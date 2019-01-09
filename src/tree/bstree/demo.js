4f68f8147c7f211e8998e22a867fe1c22db5892b
huawei.zhu <huawei.zhu@downtown8.com>
2019-01-09 17:34:28 +0800

    fix bugs

--- src/tree/bstree/demo.js
+++ src/tree/bstree/demo.js
@@ -1,11 +1,12 @@
 const Tree = require('./tree');
 const Node = require('./node');
 
-const array = [14, 5, 6, 2, 7, 3, 23, 25, 12, 15, 11, 27]
+const array = [14, 5, 6, 2, 7, 3, 23, 25, 12, 15, 11, 27, 28, 29, 30, 31, 32, 33]
 const tree = new Tree()
 
 array.forEach(node => {
 	const nodeIns = new Node(node);
 	tree.insert(nodeIns);
-	console.log(tree.acqTree())
 });
+
+console.log(JSON.stringify(tree.acqTree()))
