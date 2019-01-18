478fc9473f783fc6c114cd6dea1d114d20a76202
huawei.zhu <huawei.zhu@downtown8.com>
2019-01-18 11:06:12 +0800

    fix bugs

---
+++ src/tree/2-3-tree/node.js
@@ -0,0 +1,16 @@
+class Node {
+	constructor(value) {
+		this.leftValue = value;
+		this.rightValue = null;
+
+		this.left = null;
+		this.middle = null;
+		this.right = null;
+
+		this.parent = null;
+	}
+
+	insert(value) {
+		if ()
+	}
+}
---
+++ src/tree/2-3-tree/tree.js
@@ -0,0 +1,9 @@
+const PTree = require('../common/tree');
+
+class Tree extends PTree {
+	insert(node) {
+
+	}
+}
+
+module.exports = Tree;
